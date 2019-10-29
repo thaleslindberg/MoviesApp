30edb9814061cc3d9a38b68255dad86420ea0210
Thales <thales.lindberg@gmail.com>
2019-10-29 19:58:24 -0300

    changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCW8H26gKVvXhHVejG5W972G29f7A5NrzQ",
+    authDomain: "moviesonlineapp-7624c.firebaseapp.com",
+    databaseURL: "https://moviesonlineapp-7624c.firebaseio.com",
+    projectId: "moviesonlineapp-7624c",
+    storageBucket: "moviesonlineapp-7624c.appspot.com",
+    messagingSenderId: "1007994866365",
+    appId: "1:1007994866365:web:ed84d1dfaa8455bf50c5d4",
+    measurementId: "G-ZK6J6GMG97"
+  }
 };
 
 /*
